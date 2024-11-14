1b9c970f70c7ce55546b96b271a12e2bc60a7fdc
Alex <www.inema9886@gmail.com>
2024-11-14 20:56:50 +0100

    Add LoadingSpinner.tsx

---
+++ components/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+// components/LoadingSpinner.tsx
+import React from 'react';
+
+const LoadingSpinner: React.FC = () => (
+  <div className="loader w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+);
+
+export default LoadingSpinner;
